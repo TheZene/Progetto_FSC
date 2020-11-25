ba59d919fd22f578ec10588b10185d43048170fc
Prop4et <francostanco97@gmail.com>
2020-11-25 12:43:41 +0100

    framerate cap

--- PesciSferici/main.cpp
+++ PesciSferici/main.cpp
@@ -1,15 +1,22 @@
 #include <FL/Fl.H>
 #include <stdio.h>
 
+#include <thread>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <deque>
+
 #include "form.h"
 #include "frame.h"
-
+#include "School.h"
+#include "Potential.h"
+#include "define.h"
 using namespace std;
 //
 extern Frame* scene;
-//-------------------------------------------------------------------------------------------------
 void idle_cb(void*)
-{	
+{
 	scene->redraw();
 	Fl::repeat_timeout(1/120, idle_cb);
 }
@@ -20,6 +27,4 @@ int main(int argc, char** argv) {
 	Fl::add_timeout(1/120, idle_cb);
 	Fl::add_idle(idle_cb, 0);
 	return Fl::run();
-	
-
 }
