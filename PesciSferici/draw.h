bfbb432a98509150218f0aed826453b7c11506fa
Prop4et <francostanco97@gmail.com>
2020-11-25 12:40:48 +0100

    "framerate cap"

--- PesciSferici/draw.h
+++ PesciSferici/draw.h
@@ -11,22 +11,21 @@ void draw_scene(void);
 //prova per potenziale repulsivo+attrattivo.
 float pos1[3] = { 0  ,10, 1.0 };
 float vel1[3] = { 0, -6, 0.2 };
-
 float pos2[3] = {0, 0, 0 };
-float vel2[3] = { 0.3, 0.7, 0.1 };
+float vel2[3] = { 0.3, 0.7, 0.1 };/*
 
 float pos3[3] = { 3, -3, -0.2 };
 float vel3[3] = { -0.3, 0.2, 0.3 };
 
 float pos4[3] = { 2., -0.3, 0.5 };
-float vel4[3] = { 1, 0.8, -0.3 };
+float vel4[3] = { 1, 0.8, -0.3 };*/
 
 Fish uno (pos1, vel1);
-Fish due(pos2, vel2);
+Fish due(pos2, vel2);/*
 Fish tre(pos3, vel3);
-Fish quattro(pos4, vel4);
+Fish quattro(pos4, vel4);*/
 School un(&uno);
-School du(&due);
+School du(&due);/*
 School tr(&tre);
-School qu(&quattro);
-vector<School> pozza = { un, du, tr, qu};
+School qu(&quattro);*/
+vector<School> pozza = { un, du/*, tr, qu*/};
--- PesciSferici/main.cpp
+++ PesciSferici/main.cpp
@@ -11,13 +11,13 @@ extern Frame* scene;
 void idle_cb(void*)
 {	
 	scene->redraw();
-	//Fl::repeat_timeout(1, idle_cb);
+	Fl::repeat_timeout(1/120, idle_cb);
 }
 // ********************************************************************************************************
 
 int main(int argc, char** argv) {
 	CreateMyWindow();
-	//Fl::add_timeout(1, idle_cb);
+	Fl::add_timeout(1/120, idle_cb);
 	Fl::add_idle(idle_cb, 0);
 	return Fl::run();
 	
